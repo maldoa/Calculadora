2cf92d8c20366b5e44f3264f8b04c1b120e51ad9
maldoa <54679642+maldoa@users.noreply.github.com>
2019-09-19 09:37:37 -0500

    Add files via upload

---
+++ app/src/main/java/mx/ipn/cic/calculadora/Bloque2Fragment.java
@@ -0,0 +1,29 @@
+package mx.ipn.cic.calculadora;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Bloque2Fragment extends Fragment {
+
+    public Bloque2Fragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bloque2, container, false);
+    }
+
+
+}
