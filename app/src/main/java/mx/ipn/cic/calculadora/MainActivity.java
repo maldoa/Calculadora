1bcca8439bd97573f36a3fc721fd6072e90c5a56
Alberto Maldonado <amaldonador1300@alumno.ipn.mx>
2019-09-24 09:37:19 -0500

    final project

--- app/src/main/java/mx/ipn/cic/calculadora/MainActivity.java
+++ app/src/main/java/mx/ipn/cic/calculadora/MainActivity.java
@@ -2,27 +2,73 @@ package mx.ipn.cic.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
-import mx.ipn.cic.calculadora.posfijo.Convertidor;
+import mx.ipn.cic.calculadora.convertidor.Convertidor;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Convertidor conv = new Convertidor();
-
+    Convertidor conv;
+    TextView tv;
+    String texto = "", operacion = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+        tv = (TextView)findViewById(R.id.textoCalculadora);
+
     }
 
     @Override
     public void onClick(View view) {
-        Log.d("Acción",((Button)view).getText().toString());
-        Toast.makeText(getApplicationContext(),((Button)view).getText().toString(),Toast.LENGTH_LONG).show();
-        Log.d("resu", conv.Ejemplo());
+       // Log.d("Acción",((Button)view).getText().toString());
+
+
+        operacion = ((Button)view).getText().toString();
+        if(operacion.equals("+/-"))
+            operacion = "-";
+        if (operacion.substring(operacion.length() - 1).equals("."))
+            operacion = operacion;
+        else
+            if (!isNumeric(operacion.substring(operacion.length() - 1)))
+                operacion = " "+operacion+" ";
+
+        texto += operacion;
+        if(((Button)view).getText().toString().equals("C"))
+            texto = "";
+        if(((Button)view).getText().toString().equals("=")) {
+            texto = texto.substring(0,texto.length()-2);
+            conv = new Convertidor(texto);
+           // Toast.makeText(getApplicationContext(),texto,Toast.LENGTH_LONG).show();
+            texto = conv.transforma();
+            Toast.makeText(getApplicationContext(),texto,Toast.LENGTH_LONG).show();
+            String[] strArr = texto.split(" ");
+            texto = " "  + conv.calculator(strArr);
+        }
+        tv.setText(texto.replace(" ",""));
+
+        Log.d("Acción",texto.replace(" ",""));
+
+            //else
+        //Toast.makeText(getApplicationContext(),((Button)view).getText().toString(),Toast.LENGTH_LONG).show();
+        //Log.d("resu", conv.Ejemplo());
+
+
+
+    }
+
+    public  boolean isNumeric(String strNum) {
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/mx/ipn/cic/calculadora/convertidor/Convertidor.java
+++ app/src/main/java/mx/ipn/cic/calculadora/convertidor/Convertidor.java
@@ -1,83 +1,187 @@
+package mx.ipn.cic.calculadora.convertidor;
+
 import java.io.*;
 import java.util.*;
- 
+import java.lang.Math;
+
 public class Convertidor {
     private Stack stack;
-
     private StringTokenizer str;
     private String salida = "";
-
     public Convertidor (String entrada){
+
         str = new StringTokenizer(entrada);
         stack = new Stack();
     }
-      
+
     public String transforma() {
         while(str.hasMoreTokens()){
-            String token = str.nextToken();
-            switch(token) {
+            String ch = str.nextToken();
+            System.out.println(ch);
+            switch(ch) {
                 case "+":
                 case "-":
                 case "*":
                 case "/":
-                    operador(token, 1);
+                case "^":
+                case "Sen":
+                case "Cos":
+                case "Tan":
+                case "Sec":
+                case "Csc":
+                case "Cot":
+                case "Ln":
+                    oper(ch, 1);
                     break;
                 case "(":
-                    stack.push(token);
+                    stack.push(ch);
                     break;
                 case ")":
-                    parentesis(token);
+                    paren(ch);
+                    break;
+                case ".":
+                    salida = salida + ch;
                     break;
                 default:
-                    salida = salida + " " + token;
+                    salida = salida + " " + ch;
                     break;
             }
         }
-          
+
         while(!stack.empty()){
-            salida = salida + " " + stack.pop();
+            salida = salida + " " +stack.pop();
         }
-          
+
+        System.out.println(salida);
         return salida;
     }
-
-    public void operador(String token, int valor1){
+    public void oper(String opEsto, int prec1){
         while(!stack.empty()) {
-            token =(String) stack.pop();
-            if(token == "("){
-                stack.push(token);
+            String opTop = (String) stack.pop();
+            if(opTop.equals("(")){
+                stack.push(opTop);
                 break;
             }else{
-                int valor2;
-                if(token == "+" || token == "-" || token == "*" || token == "/")
-                    valor2 = 1;
+                int prec2;
+                if(opTop == "+" || opTop == "-" || opTop == "*"|| opTop == "/")
+                    prec2 = 1;
                 else
-                    valor2 = 2;
-                if(valor2 < valor1) {
-                    stack.push(token);
+                    prec2 = 2;
+                if(prec2 < prec1) {
+                    stack.push(opTop);
                     break;
                 }else
-                    salida = salida + " " + token;
+                    salida = salida + " " + opTop;
             }
         }
-        stack.push(token);
+        stack.push(opEsto);
     }
-      
-    public void parentesis(String token){
+
+    public void paren(String ch){
         while (!stack.empty()) {
-            token = (String) stack.pop();
-            if (token == "(")
+            String chx = (String) stack.pop();
+            if (chx.equals("("))
                 break;
-            else{
-                salida = salida + token+ " )";
-                }
+            else
+                salida = salida + " " + chx;
         }
     }
-      
-    public static void main(String[] args) {
+
+/*    public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         System.out.println("Ingresa una expresion infija");
-        Convertidor exp = new Convertidor(leer.nextLine());
-        System.out.println("Posfijo es " + exp.transforma());
+        String entrada = leer.nextLine();
+        String salida;
+        Convertidor exp = new Convertidor(entrada);
+        salida = exp.transforma();
+        System.out.println("Posfijo es " + salida);
+        String ejemplo = "1.87 3.654 + 4 * Sen";
+        String[] strArr = salida.split(" ");
+
+        System.out.println(calculator(strArr));
+    }*/
+
+    public  double calculator(String[] strArr) {
+        Stack<Double> operands = new Stack<Double>();
+
+        for(String str : strArr) {
+            if (str.trim().equals("")) {
+                continue;
+            }
+
+            switch (str) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "^":
+                    double right = operands.pop();
+                    double left = operands.pop();
+                    double value = 0;
+                    switch(str) {
+                        case "+":
+                            value = left + right;
+                            break;
+                        case "-":
+                            value = left - right;
+                            break;
+                        case "*":
+                            value = left * right;
+                            break;
+                        case "/":
+                            value = left / right;
+                            break;
+                        case "^":
+                            value = Math.pow(left, right);
+                            break;
+                        default:
+                            break;
+                    }
+                    operands.push(value);
+                    break;
+                case "Sen":
+                case "Cos":
+                case "Tan":
+                case "Sec":
+                case "Csc":
+                case "Cot":
+                case "Ln":
+                    right = operands.pop();
+                    value = 0;
+                    switch(str) {
+
+                        case "Sen":
+                            value = Math.sin(right);
+                            break;
+                        case "Cos":
+                            value = Math.cos(right);
+                            break;
+                        case "Tan":
+                            value = Math.tan(right);
+                            break;
+                        case "Cot":
+                            value = 1/ Math.tan(right);
+                            break;
+                        case "Csc":
+                            value = 1/ Math.sin(right);
+                            break;
+                        case "Sec":
+                            value = 1 / Math.cos(right);
+                            break;
+                        case "Ln":
+                            value = Math.log(right);
+                            break;
+                        default:
+                            break;
+                    }
+                    operands.push(value);
+                    break;
+
+                default:
+                    operands.push(Double.parseDouble(str));
+                    break;
+            }
+        }
+        return operands.pop();
     }
 }
